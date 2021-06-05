0ac069f15acd5e26be3db4380cfba62f73f9e9b6
xuelongqy <59814509@qq.com>
2021-06-06 00:29:00 +0800

    feat: 添加Linux支持

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_jscore/flutter_jscore_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_jscore_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterJscorePlugin");
+  flutter_jscore_plugin_register_with_registrar(flutter_jscore_registrar);
+}
--- lib/binding/jsc_ffi.dart
+++ lib/binding/jsc_ffi.dart
@@ -2,7 +2,10 @@ import 'dart:ffi';
 import 'dart:io';
 
 final DynamicLibrary jscLib = Platform.isIOS || Platform.isMacOS
-    ? DynamicLibrary.open("JavaScriptCore.framework/JavaScriptCore")
+    ? DynamicLibrary.open('JavaScriptCore.framework/JavaScriptCore')
     : Platform.isWindows
-        ? DynamicLibrary.open("JavaScriptCore.dll")
-        : DynamicLibrary.open("libjsc.so");
+        ? DynamicLibrary.open('JavaScriptCore.dll')
+        : Platform.isLinux
+            ? DynamicLibrary.open(
+                '${File(Platform.resolvedExecutable).parent.path}/lib/libjsc.so')
+            : DynamicLibrary.open('libjsc.so');
